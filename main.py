47e3add87ff14654e0e6b52a19012c75219d3ad8
BaronKhan <bksonic@hotmail.co.uk>
2017-06-27 22:24:36 +0100

    Add espeak to main.py and update .gitignore

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from src import houndify
 from src import client_defines
+import os
 
 import sys
 
+def play_voice(voice_text):
+    voice_lang="-ven-us"
+    voice_speed="-s175" # default: 175
+    voice_type="m7"     # current: m7
+    voice_gap=""
+    os.system("espeak "+voice_lang +"+"+voice_type+" "+voice_speed+" "+voice_gap+" \""+voice_text+"\" 2>/dev/null")
+
 if __name__ == '__main__':
   print("client id: "+client_defines.CLIENT_ID+"\nclient key: "+client_defines.CLIENT_KEY)
+  voice_text = "Hello. I am fir. Nice to meet you."
+  play_voice(voice_text)
+  voice_text = "The temperature is sixteen degrees celsius."
+  play_voice(voice_text)
